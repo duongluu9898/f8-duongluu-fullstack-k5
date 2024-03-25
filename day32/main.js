477752574c38b50d1123f391f3f6c82bcbeebde3
Your Name <you@example.com>
2024-03-25 13:20:57 +0700

    nop bai tap buoi 32

--- day32/main.js
+++ day32/main.js
@@ -9,55 +9,58 @@ items.forEach(function (item) {
   //add ghost
   var dragStart = item.addEventListener("dragstart", function (e) {
     dragItem = e.currentTarget;
+    console.log(dragItem);
     e.currentTarget.classList.add("ghost");
   });
 
   //drag
-  var dragOver = item.addEventListener("dragover", function (e) {
+  var dragOver = item.addEventListener("dragenter", function (e) {
     e.preventDefault();
     targetEl = e.currentTarget;
     var targetSpace = targetEl.getBoundingClientRect();
 
     var y = e.clientY;
     var targetTop = targetSpace.top;
-    var relativey = y - targetTop;
+    var spaceY = y - targetTop;
 
-    if (relativey < targetSpace.height / 2) {
+    if (spaceY < targetSpace.height) {
       list.insertBefore(dragItem, targetEl);
     } else {
       list.insertBefore(dragItem, targetEl.nextSibling);
     }
   });
+});
 
-  //drop
-  var drop = function (e) {
-    item.addEventListener("drop", function (e) {
-      dragItem.classList.remove("ghost");
-      items = document.querySelectorAll(".item");
+var addDragend = function (item) {
+  item.addEventListener("dragend", dragend);
+};
 
-      var indexCourse = 1;
-      var indexModule = 1;
-      items.forEach(function (item) {
-        if (item.children[0].innerText.includes("Module")) {
-          item.children[0].innerText = `Module + ${indexModule} + :`;
-          indexModule++;
-          console.log(indexModule);
-        } else {
-          item.children[0].innerText = "Bài" + indexCourse + ":";
-          indexCourse++;
-          console.log(indexCourse);
-        }
-      });
-    });
-  };
+var dragend = function (item) {
+  dragItem.classList.remove("ghost");
+  var items = document.querySelectorAll(".item");
+  //   console.log(items);
+
+  var indexCourse = 1;
+  var indexModule = 1;
+  items.forEach(function (item) {
+    if (item.children[0].innerText.includes("Module")) {
+      item.children[0].innerText = `Module ${indexModule} :`;
+      indexModule++;
+      //   console.log(indexModule);
+    } else {
+      item.children[0].innerText = `Bài ${indexCourse} :`;
+      indexCourse++;
+      //   console.log(indexCourse);
+    }
+  });
+};
+
+items.forEach(function (item) {
+  addDragend(item);
 });
 
 list.addEventListener("mousedown", function (e) {
   if (e.target.className === "list") {
     e.preventDefault();
   }
-});
-
-list.addEventListener("dragend", function (e) {
-  dragItem.classList.remove("ghost");
 });
