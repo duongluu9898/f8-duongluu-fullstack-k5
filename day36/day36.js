c05e4c0822046935ae370332ea4f582cbf2d1f73
Your Name <you@example.com>
2024-04-08 15:41:41 +0700

    nop bai tap buoi 36

--- day36/day36.js
+++ day36/day36.js
@@ -1,3 +1,4 @@
+//Prefixed properties
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const SpeechGrammarList =
@@ -5,47 +6,85 @@ const SpeechGrammarList =
 const SpeechRecognitionEvent =
   window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
 
-const grammar = "#JSGF V1.0;";
+//grammer
+const grammar = `#JSGF V1.0;`;
+
+//speech recognition
 const recognition = new SpeechRecognition();
 const speechRecognitionList = new SpeechGrammarList();
-speechRecognitionList.addFromString(grammar, 1);
-recognition.grammars = speechRecognitionList;
 
+//add grammer
+speechRecognitionList.addFromString(grammar, 1);
+
+//plugging grammer into speechRecognition
+recognition.grammars = speechRecognitionList;
 recognition.continuous = false;
 recognition.lang = "vi-vn";
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+//get btn, action
 const btn = document.querySelector(".btn");
 const action = document.querySelector(".action");
-console.log(btn);
-console.log(action);
+// console.log(btn);
+// console.log(action);
 
 //add event click btn
 btn.onclick = () => {
   recognition.start();
-  action.style.display = "block";
   console.log("Sẵn sàng nhận lệnh bằng giọng nói");
+
+  //add action
+  action.style.display = "block";
 };
 
-//result
+//Receiving and handling results
 recognition.onresult = (event) => {
   const text = event.results[0][0].transcript;
-  const textValue = text.toLowerCase();
+  diagnostic.textContent = `Result received: ${text}.`;
+  console.log(`Confidence: ${event.results[0][0].confidence}`);
 
-  if (textValue.includes("google")) {
+  //   console.log(`Kết quả nhận được: ${text}.`);
+  //   console.log(text);
+  //   console.log(event.result[0]);
+
+  //google
+  if (text.includes("google")) {
     window.open("https://www.google.com", "_blank");
-  } else if (textValue.includes("facebook")) {
+
+    //facebook
+  } else if (text.includes("facebook")) {
     window.open("https://www.facebook.com", "_blank");
-  } else if (textValue.includes("youtube")) {
+
+    //youtube
+  } else if (text.includes("youtube")) {
     window.open("https://www.youtube.com", "_blank");
-  } else if (textValue.includes("google drive")) {
+
+    //google driver
+  } else if (text.includes("google drive")) {
     window.open("https://www.drive.google.com", "_blank");
+
+    //google map
+  } else if (text.includes("googles map")) {
+    window.open(
+      "https://www.google.com/maps/@9.779349,105.6189045,11z?hl=vi-VN&entry=ttu",
+      "_blank"
+    );
   }
 };
-//end speech
 
+//end speech
 recognition.onspeechend = () => {
   recognition.stop();
   action.style.display = "none";
+};
+
+//Handling errors and unrecognized speech
+recognition.onnomatch = (event) => {
+  diagnostic.textContent = "Không thực hiện được yêu cầu";
+  console.log(diagnostic.textContent);
+};
+
+recognition.onerror = (event) => {
+  diagnostic.textContent = `Error occurred in recognition: ${event.error}`;
 };
